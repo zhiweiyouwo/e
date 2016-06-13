bf82280c4345b72763412cef96528444cf33f22d
fuhoujun <21549322@qq.com>
2016-06-13 14:22:25 +0800

    e 1.0.0

---
+++ e/src/main/java/com/loy/e/tools/Options.java
@@ -0,0 +1,15 @@
+package com.loy.e.tools;
+
+/**
+ * 
+ * @author Loy Fu qq群 540553957
+ * @since 1.7
+ * @version 1.0.0
+ * 
+ */
+public class Options {
+
+	public boolean debug = false;
+	public String projectPath = null;
+	
+}
