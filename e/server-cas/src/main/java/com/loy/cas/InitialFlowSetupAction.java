4c889a9858922f978afad4d28898a55db5a07227
fuhoujun <21549322@qq.com>
2016-08-24 09:11:12 +0800

    e 2.0.0

---
+++ e/server-cas/src/main/java/com/loy/cas/InitialFlowSetupAction.java
@@ -0,0 +1,89 @@
+package com.loy.cas;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.jasig.cas.authentication.principal.Service;
+import org.jasig.cas.web.support.ArgumentExtractor;
+import org.jasig.cas.web.support.CookieRetrievingCookieGenerator;
+import org.jasig.cas.web.support.WebUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.webflow.action.AbstractAction;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
+/**
+ * 
+ * @author Loy Fu qq群 540553957
+ * @since 1.7
+ * @version 1.0.0
+ * 
+ */
+public class InitialFlowSetupAction extends AbstractAction
+{
+
+	  @NotNull
+	  private CookieRetrievingCookieGenerator warnCookieGenerator;
+
+	  @NotNull
+	  private CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator;
+
+	  @NotNull
+	  @Size(min=1)
+	  private List<ArgumentExtractor> argumentExtractors;
+	  private boolean pathPopulated = false;
+
+	  protected Event doExecute(RequestContext context) throws Exception
+	  {
+	    HttpServletRequest request = WebUtils.getHttpServletRequest(context);
+	    if (!this.pathPopulated) {
+	      String contextPath = context.getExternalContext().getContextPath();
+	      String cookiePath = StringUtils.hasText(contextPath) ? contextPath + "/" : "/";
+	      this.logger.info("Setting path for cookies to: " + cookiePath);
+
+	      this.warnCookieGenerator.setCookiePath(cookiePath);
+	      this.ticketGrantingTicketCookieGenerator.setCookiePath(cookiePath);
+	      this.pathPopulated = true;
+	    }
+
+	    context.getFlowScope().put("ticketGrantingTicketId", this.ticketGrantingTicketCookieGenerator.retrieveCookieValue(request));
+
+	    context.getFlowScope().put("warnCookieValue", Boolean.valueOf(this.warnCookieGenerator.retrieveCookieValue(request)));
+
+	    Service service = WebUtils.getService(this.argumentExtractors, context);
+
+	    if ((service != null) && (this.logger.isDebugEnabled())) {
+	      this.logger.debug("Placing service in FlowScope: " + service.getId());
+	    }
+
+	    context.getFlowScope().put("service", service);
+
+	    return result("success");
+	  }
+
+	  public void setTicketGrantingTicketCookieGenerator(CookieRetrievingCookieGenerator ticketGrantingTicketCookieGenerator)
+	  {
+	    this.ticketGrantingTicketCookieGenerator = ticketGrantingTicketCookieGenerator;
+	  }
+
+	  public void setWarnCookieGenerator(CookieRetrievingCookieGenerator warnCookieGenerator) {
+	    this.warnCookieGenerator = warnCookieGenerator;
+	  }
+
+	  public void setArgumentExtractors(List<ArgumentExtractor> argumentExtractors)
+	  {
+	    this.argumentExtractors = argumentExtractors;
+	  }
+
+	public boolean isPathPopulated() {
+		return pathPopulated;
+	}
+
+	public void setPathPopulated(boolean pathPopulated) {
+		this.pathPopulated = pathPopulated;
+	}
+	  
+	  
+	}
---
+++ e/server-zkp/src/main/java/com/loy/e/zkp/server/ZookeeperMain.java
@@ -0,0 +1,43 @@
+package com.loy.e.zkp.server;
+
+
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig.ConfigException;
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+
+/**
+ * 
+ * 
+ * @author Loy Fu qq群 540553957
+ * @since 1.7
+ * @version 1.0.0
+ *
+ */
+public class ZookeeperMain extends   QuorumPeerMain{
+	private static final Logger LOG = LoggerFactory.getLogger(ZookeeperMain.class);
+	public static void main(String[] args){
+	        try {
+	        	PropertyConfigurator.configure(ResourceUtils.getFile("classpath:conf/log4j.properties").getAbsolutePath());
+	        	ZookeeperMain main = new ZookeeperMain();
+	        	String path = ResourceUtils.getFile("classpath:conf/zoo.cfg").getAbsolutePath();
+	        	String[] arg =  {path};
+	            main.initializeAndRun(arg);
+	        } catch (IllegalArgumentException e) {
+	            LOG.error("Invalid arguments, exiting abnormally", e);
+	            System.exit(2);
+	        } catch (ConfigException e) {
+	            LOG.error("Invalid config, exiting abnormally", e);
+	            System.err.println("Invalid config, exiting abnormally");
+	            System.exit(2);
+	        } catch (Exception e) {
+	            LOG.error("Unexpected exception, exiting abnormally", e);
+	            System.exit(1);
+	        }
+	        LOG.info("Exiting normally");
+	        System.exit(0);
+		
+	}
+}
